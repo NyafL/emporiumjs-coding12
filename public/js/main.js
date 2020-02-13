2e88265d790a6121e24618e000f698342a8c2faa
Luca <luca@szoftverek.hu>
2020-02-13 15:19:21 +0100

    carousel almost one

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ console.log(brand);
 
 brand.style.display = "none"
 
-    
+
 //  window.onscroll = function(){
 //         brand.style.display = "block"
 //         emporium.style.display = "none"
@@ -29,7 +29,7 @@ brand.style.display = "none"
 // });
 
 
-   
+
 
 
 var modal = document.getElementById("myModal");
@@ -41,21 +41,21 @@ var btn = document.getElementsByClassName('connection')[0]
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.addEventListener('click', () => { 
+btn.addEventListener('click', () => {
   modal.style.display = "block";
 })
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-} 
+}
 
 //website color change 
 let whitebtn = document.getElementById('whitebtn')
@@ -75,11 +75,11 @@ let card8 = document.getElementsByClassName('card')[10]
 let card9 = document.getElementsByClassName('card')[11]
 
 let card10 = document.getElementsByClassName('card')[0]
-let card11= document.getElementsByClassName('card')[1]
+let card11 = document.getElementsByClassName('card')[1]
 let card12 = document.getElementsByClassName('card')[2]
 
 let card13 = document.getElementsByClassName('card')[12]
-let card14= document.getElementsByClassName('card')[13]
+let card14 = document.getElementsByClassName('card')[13]
 let card15 = document.getElementsByClassName('card')[14]
 
 let section1 = document.getElementsByTagName('section')[0]
@@ -88,29 +88,29 @@ let footer = document.getElementsByTagName('footer')[0]
 
 
 whitebtn.addEventListener('click', () => {
-    nav.className = "navbar sticky-top navbar-expand-lg navbar-light bg-white "
-    // h1.style.backgroundColor = "white"
-    // h1.style.color = "black"
-    body.style.backgroundColor = "white"
-    body.style.color = "black"
-    card1.classList.add('bg-white')  
-    //recents products
-    card2.classList.add('bg-white')
-    card3.classList.add('bg-white')
-    card4.classList.add('bg-white')
-    card5.classList.add('bg-white')
-    card6.classList.add('bg-white')
-    card7.classList.add('bg-white')
-    card8.classList.add('bg-white')
-    card9.classList.add('bg-white')
-    card10.classList.add('bg-white')
-    card11.classList.add('bg-white')
-    card12.classList.add('bg-white')
-    card13.classList.add('bg-white')
-    card14.classList.add('bg-white')
-    card15.classList.add('bg-white')
+  nav.className = "navbar sticky-top navbar-expand-lg navbar-light bg-white "
+  // h1.style.backgroundColor = "white"
+  // h1.style.color = "black"
+  body.style.backgroundColor = "white"
+  body.style.color = "black"
+  card1.classList.add('bg-white')
+  //recents products
+  card2.classList.add('bg-white')
+  card3.classList.add('bg-white')
+  card4.classList.add('bg-white')
+  card5.classList.add('bg-white')
+  card6.classList.add('bg-white')
+  card7.classList.add('bg-white')
+  card8.classList.add('bg-white')
+  card9.classList.add('bg-white')
+  card10.classList.add('bg-white')
+  card11.classList.add('bg-white')
+  card12.classList.add('bg-white')
+  card13.classList.add('bg-white')
+  card14.classList.add('bg-white')
+  card15.classList.add('bg-white')
 
-    section1.classList.add('bg-white')
+  section1.classList.add('bg-white')
 
 
 
@@ -119,44 +119,76 @@ whitebtn.addEventListener('click', () => {
 })
 
 blackbtn.addEventListener('click', () => {
-    nav.className = "navbar sticky-top navbar-expand-lg navbar-dark bg-dark "
-    // h1.style.backgroundColor = "black"
-    // h1.style.color = "white"
-    body.style.backgroundColor = "black"
-    body.style.color = "white"
-    card1.classList.add('bg-dark')  
-    card1.classList.remove('bg-white')
-    //recents products
-    card2.classList.add('bg-dark')
-    card2.classList.remove('bg-white')
-    card3.classList.add('bg-dark')
-    card3.classList.remove('bg-white')
-    card4.classList.add('bg-dark')
-    card4.classList.remove('bg-white')
-    card5.classList.add('bg-dark')
-    card5.classList.remove('bg-white')
-    card6.classList.add('bg-dark')
-    card6.classList.remove('bg-white')
-    card7.classList.add('bg-dark')
-    card7.classList.remove('bg-white')
-    card8.classList.add('bg-dark')
-    card8.classList.remove('bg-white')
-    card9.classList.add('bg-dark')
-    card9.classList.remove('bg-white')
-    card10.classList.add('bg-dark')
-    card10.classList.remove('bg-white')
-    card11.classList.add('bg-dark')
-    card11.classList.remove('bg-white')
-    card12.classList.add('bg-dark')
-    card13.classList.remove('bg-white')
-    card13.classList.add('bg-dark')
-    card13.classList.remove('bg-white')
-    card14.classList.add('bg-dark')
-    card14.classList.remove('bg-white')
-    card15.classList.add('bg-dark')
-    card15.classList.remove('bg-white')
+  nav.className = "navbar sticky-top navbar-expand-lg navbar-dark bg-dark "
+  // h1.style.backgroundColor = "black"
+  // h1.style.color = "white"
+  body.style.backgroundColor = "black"
+  body.style.color = "white"
+  card1.classList.add('bg-dark')
+  card1.classList.remove('bg-white')
+  //recents products
+  card2.classList.add('bg-dark')
+  card2.classList.remove('bg-white')
+  card3.classList.add('bg-dark')
+  card3.classList.remove('bg-white')
+  card4.classList.add('bg-dark')
+  card4.classList.remove('bg-white')
+  card5.classList.add('bg-dark')
+  card5.classList.remove('bg-white')
+  card6.classList.add('bg-dark')
+  card6.classList.remove('bg-white')
+  card7.classList.add('bg-dark')
+  card7.classList.remove('bg-white')
+  card8.classList.add('bg-dark')
+  card8.classList.remove('bg-white')
+  card9.classList.add('bg-dark')
+  card9.classList.remove('bg-white')
+  card10.classList.add('bg-dark')
+  card10.classList.remove('bg-white')
+  card11.classList.add('bg-dark')
+  card11.classList.remove('bg-white')
+  card12.classList.add('bg-dark')
+  card12.classList.remove('bg-white')
+  card13.classList.remove('bg-white')
+  card13.classList.add('bg-dark')
+  card13.classList.remove('bg-white')
+  card14.classList.add('bg-dark')
+  card14.classList.remove('bg-white')
+  card15.classList.add('bg-dark')
+  card15.classList.remove('bg-white')
 
-    section1.classList.add('bg-dark')
-    section1.classList.remove('bg-white')
+  section1.classList.add('bg-dark')
+  section1.classList.remove('bg-white')
 
 })
+
+
+//carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
